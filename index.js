daabd1ba74b0bb0da21d38b037b9f46d315b45ac
caleb <kalebntoyian2004@gmail.com>
2024-07-23 15:41:02 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+// index.js
+
+// Initialize the cats array
+var cats = ["Milo", "Otis", "Garfield"];
+
+// Function to append a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// Function to prepend a cat to the beginning of the cats array
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// Function to remove the last cat from the cats array
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// Function to remove the first cat from the cats array
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Function to append a cat to the cats array and return a new array
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// Function to prepend a cat to the cats array and return a new array
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// Function to remove the last cat from the cats array and return a new array
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+// Function to remove the first cat from the cats array and return a new array
+function removeFirstCat() {
+  return cats.slice(1);
+}
